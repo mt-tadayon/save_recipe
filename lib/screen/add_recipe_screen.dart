93356e618b9f98704b5ff38da14780782c189edb
myracledesign <weber.max.1988@live.de>
2020-06-25 14:50:35 +0200

    session 4 finished

--- lib/screen/add_recipe_screen.dart
+++ lib/screen/add_recipe_screen.dart
@@ -23,6 +23,7 @@ class _AddRecipeScreenState extends State<AddRecipeScreen> {
   final _formKey = GlobalKey<FormState>();
   String dropdownValue;
   File _image;
+  bool isImageSelected = false;
 
   @override
   void initState() {
@@ -82,6 +83,13 @@ class _AddRecipeScreenState extends State<AddRecipeScreen> {
                                     decoration: InputDecoration.collapsed(
                                       hintText: 'Recipe Name',
                                     ),
+                                    validator: (value) {
+                                      if (value.isEmpty) {
+                                        return 'Please provide a recipe name';
+                                      }
+                                      return null;
+                                    },
+                                    keyboardType: TextInputType.text,
                                   ),
                                   decoration: BoxDecoration(
                                       color: kWhite,
@@ -125,31 +133,60 @@ class _AddRecipeScreenState extends State<AddRecipeScreen> {
                                 }),
                                 SizedBox(height: 20),
                                 Container(
-                                    height: MediaQuery.of(context).size.height *
-                                        0.30,
-                                    width: double.infinity,
-                                    decoration: BoxDecoration(
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.30,
+                                  width: double.infinity,
+                                  decoration: BoxDecoration(
                                       color: kWhite,
                                       borderRadius: BorderRadius.circular(30),
                                       border: Border.all(
                                         color: kBorderColor,
                                       ),
-                                    ),
-                                    child: _image == null
-                                        ? IconButton(
-                                            icon: Icon(
-                                              Icons.camera,
-                                              size: 60.0,
-                                            ),
-                                            onPressed: () async {
-                                              _image =
-                                                  await ImagePicker.pickImage(
-                                                      source:
-                                                          ImageSource.gallery);
-                                              setState(() {});
-                                            },
-                                          )
-                                        : Image.file(_image)),
+                                      image: _image != null
+                                          ? DecorationImage(
+                                              image: FileImage(
+                                                _image,
+                                              ),
+                                              fit: BoxFit.cover,
+                                              colorFilter: ColorFilter.mode(
+                                                  Colors.black.withOpacity(0.5),
+                                                  BlendMode.dstATop),
+                                            )
+                                          : null),
+                                  child: Stack(
+                                    children: <Widget>[
+                                      Center(
+                                        child: IconButton(
+                                          icon: Icon(
+                                            Icons.camera,
+                                          ),
+                                          iconSize: 60,
+                                          onPressed: () async {
+                                            _image =
+                                                await ImagePicker.pickImage(
+                                                    source:
+                                                        ImageSource.gallery);
+                                            setState(() {});
+                                          },
+                                        ),
+                                      ),
+                                      isImageSelected
+                                          ? Align(
+                                              alignment: Alignment.bottomCenter,
+                                              child: Padding(
+                                                padding:
+                                                    const EdgeInsets.all(8.0),
+                                                child: Text(
+                                                  'Please select an image',
+                                                  style: TextStyle(
+                                                      color: Colors.red),
+                                                ),
+                                              ),
+                                            )
+                                          : Container()
+                                    ],
+                                  ),
+                                ),
                               ],
                             ),
                             SizedBox(height: 20),
@@ -164,7 +201,15 @@ class _AddRecipeScreenState extends State<AddRecipeScreen> {
                                 style: TextStyle(color: kWhite, fontSize: 18),
                               ),
                               color: Theme.of(context).buttonColor,
-                              onPressed: () {},
+                              onPressed: () {
+                                isImageSelected = _image == null;
+                                setState(() {});
+                                if (_formKey.currentState.validate() &&
+                                    !isImageSelected) {
+                                  //TODO: save data on db
+                                  Navigator.pop(context);
+                                }
+                              },
                             )
                           ],
                         ),
